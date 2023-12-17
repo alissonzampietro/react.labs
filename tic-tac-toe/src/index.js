837012de9ff59ac304a5d2c3bc12ef1bc29d8cab
Alisson Zampietro <alisson.zampietro@gmail.com>
2023-12-17 22:34:22 +0000

    finish basic working game

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -14,13 +14,15 @@ const Square = (props) => (
       super(props);
       this.state = {
         squares: Array(9).fill(null),
-        xIsNext: true
+        xIsNext: true,
       }
     }
 
     handleClick(position) {
       const currentGame = this.state.squares.slice();
-      console.log(this.state.squares);
+      if (calculateWinner(currentGame) || currentGame[position]) {
+        return;
+      }
       currentGame[position] = this.state.xIsNext ? 'X' : 'O';
       console.log(this.state.squares, currentGame);
       this.setState({squares: currentGame, xIsNext: !this.state.xIsNext})
@@ -31,7 +33,13 @@ const Square = (props) => (
     }
   
     render() {
-      const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -70,6 +78,26 @@ const Square = (props) => (
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
