c297fa1fa3c32008ba9910a120ce094d71738686
Alisson Zampietro <alisson.zampietro@gmail.com>
2024-01-09 10:58:24 +0000

    sending down the parameter color

--- side-project/src/components/base/ComponentArrowFunction.jsx
+++ side-project/src/components/base/ComponentArrowFunction.jsx
@@ -1,3 +1,11 @@
-export default () => <>
-    <h1>It's an arrow function component</h1>
-</>
+import { useContext } from "react"
+import { Theme } from "../../context/theme"
+
+export default () => {
+
+    let color = useContext(Theme);
+    
+    return <>
+        <h1>It's an arrow function component, and the color is {color}</h1>
+    </>
+}
