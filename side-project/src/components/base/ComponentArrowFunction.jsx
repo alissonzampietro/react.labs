f9d410dd9e31a3fcad4f30bad6f2a135193eb310
Alisson Zampietro <alisson.zampietro@gmail.com>
2024-01-09 11:00:27 +0000

    changing arrow function

--- side-project/src/components/base/ComponentArrowFunction.jsx
+++ side-project/src/components/base/ComponentArrowFunction.jsx
@@ -4,8 +4,8 @@ import { Theme } from "../../context/theme"
 export default () => {
 
     let color = useContext(Theme);
-    
+
     return <>
-        <h1>It's an arrow function component, and the color is {color}</h1>
+        <h1>It's an arrow function component, and the theme color is {color}</h1>
     </>
 }
